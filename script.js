2aa59fce3220c6889c5b17c1bcbe6dcd1f9cc2e3
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-11-13 18:07:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#float').on('click',(function(){
+      $('#content').toggleClass("hide show"); 
+      $('#toggle').toggleClass("fa-list-ul fa-times");
+    }));
+    $('#content a').on('click',(function(){
+      $('#content').toggleClass("show hide"); 
+      $('#toggle').toggleClass("fa-times fa-list-ul");
+    }));
+  });
